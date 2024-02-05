c21bd875c4c9840ab08395aa2dee03270dc6a44e
Saintcyr77 <shivam.bhatt04@gmail.com>
2024-02-05 22:52:47 +0530

    carousel implemented

--- quotesFrontend/src/App.tsx
+++ quotesFrontend/src/App.tsx
@@ -3,13 +3,27 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import MainBack from './components/MainBack'
-
+import Container from '@mui/material/Container';
+import gun from '../src/assets/gun.jpg'
+import { Typography } from '@mui/material'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <div className='page-container'>
+        <div className='logo'>
+          {/* <img src={gun} alt='Click' className='logo-image'></img> */}
+          <Typography variant='body1' className='typo-style'>
+            Becuse history is written in blood
+
+          </Typography>
+          
+       </div>
       <MainBack/>
+
+      </div>
+      
     </>
   )
 }
--- quotesFrontend/src/components/MainBack.tsx
+++ quotesFrontend/src/components/MainBack.tsx
@@ -1,9 +1,60 @@
-import React from 'react'
+import React from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+import ExampleCarouselImage from '../assets/back.jpg';
+import civil from '../assets/civil.jpg'
+import bunker from '../assets/bunker.jpg';
+import gun from '../assets/gun.jpg';
+import { useState } from 'react';
+import './main.scss';
 
 const MainBack: React.FC = () => {
-  return (
-    <div>MainBack</div>
-  )
-}
+      const [index, setIndex] = useState<number>(0);
 
-export default MainBack
+  const handleSelect = (selectedIndex:number) => {
+    setIndex(selectedIndex);
+  };
+  return (
+      <div>
+          
+     <Carousel activeIndex={index} onSelect={handleSelect} interval={1000}>
+      <Carousel.Item>
+         <img
+           className='image-fit'
+            src={ExampleCarouselImage}
+            alt="Second slide"
+          />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+         <img
+             className='image-fit'
+            src={civil}
+            alt="Second slide"
+          />
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+         <img
+              className='image-fit'
+            src={bunker}
+            alt="Second slide"
+          />
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+    </div>
+  );
+};
+
+export default MainBack;
